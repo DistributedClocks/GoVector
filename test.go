d64c0a0eb8a48de0a40081c5ccf73a5adcdd40c7
arcaneiceman <Dracula123>
2015-03-31 23:06:18 -0700

    Rudimentary Support For Multiple Executions
    Wali

--- test.go
+++ test.go
@@ -2,25 +2,24 @@ package main
 
 import "./govec"
 
-
 func main() {
-	Logger:= govec.Initialize("example", "example")
-	Logger2:= govec.Initialize("example2", "example2")
-	
+	Logger := govec.InitializeMutipleExecutions("example", "example")
+	Logger2 := govec.Initialize("example2", "example2")
+
 	sendbuf := []byte("messagepayload")
-	finalsend := Logger.PrepareSend("Sending Message",sendbuf)
-	finalsend = Logger2.PrepareSend("Sending Message2",sendbuf)
+	finalsend := Logger.PrepareSend("Sending Message", sendbuf)
+	finalsend = Logger2.PrepareSend("Sending Message2", sendbuf)
 	//send message
 	Logger2.LogLocalEvent("Message Processed2")
 	//s:= string(finalsend[:])
 	//fmt.Println(s)
-		
+
 	//receive message
-	recbuf:= Logger.UnpackReceive("receivingmsg", finalsend)
-	
+	recbuf := Logger.UnpackReceive("receivingmsg", finalsend)
+
 	Logger.LogLocalEvent("Message Processed")
 	//Logger.UnpackReceive(finalsend)
 	//s:= string(recbuf[:])
 	//fmt.Println(s)
-    finalsend = Logger.PrepareSend("Sending Message Again" , recbuf)
+	finalsend = Logger.PrepareSend("Sending Message Again", recbuf)
 }
