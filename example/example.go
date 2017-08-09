a3afd282dc8b08c42c97e155e5b939964625b93c
fruffy <fruffy@cs.ubc.ca>
2017-08-09 12:04:06 -0700

    Add Example.go

---
+++ example/example.go
@@ -0,0 +1,25 @@
+package main
+
+import "../govec"
+import "fmt"
+
+func main() {
+	Logger := govec.InitGoVector("MyProcess", "LogFile")
+
+	//In Sending Process
+
+	//Prepare a Message
+	messagepayload := []byte("samplepayload")
+	finalsend := Logger.PrepareSend("Sending Message", messagepayload)
+
+	//In Receiving Process
+
+	//receive message
+	var incommingMessage []byte
+	Logger.UnpackReceive("Received Message from server", finalsend, &incommingMessage)
+	//Can be called at any point
+	Logger.LogLocalEvent("Example Complete")
+
+	Logger.DisableLogging()
+	//No further events will be written to log file
+}
