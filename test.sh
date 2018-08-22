23cd76baeab8770adfdf61c671c2dec37e596358
vaastav <vaastav.anand05@gmail.com>
2018-08-22 14:19:24 -0700

    Update readme

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@
 set -e
 echo "" > coverage.txt
 
+# Run the coverage test on every single subdirectory
+# except the ones that are in the vendor package.
+# Currently GoVector has no vendor packages but this
+# code is left in as it is vendor-proof.
 for d in $(go list ./... | grep -v vendor); do
         go test -race -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
